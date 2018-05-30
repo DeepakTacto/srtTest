51f9b5df98dfbf2cc5cbaf26e026fbb5ddc7aba9
DeepakTacto <pdeepak45@yahoo.in>
2018-05-30 10:29:25 +0530

    first commit

---
+++ app/team.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class team extends Model
+{
+
+    public function teams() {
+        return $this->belongsTo(users);
+    }
+}
